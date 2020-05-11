3313b0f2d1f5ec1ba648c124fb50abfab7a2facb
SamilaChanu95 <chanukasamila@gmail.com>
2020-05-11 23:32:54 +0530

    Add Search Controller.

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use App\Entity\Consultant;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search")
+     */
+    public function searchAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $tabsearch = [];
+        $search = $request->request->get('recherche');
+
+        if ($search == null) {
+            $searchresult = $em->getRepository('AppBundle:User')->findAll();
+        }
+        else {
+            $searchresult = $em->getRepository('AppBundle:User')->findByUsername($search);
+        }
+
+        foreach ($searchresult as $datasearch)
+        {
+            $thisGender = $em->getRepository('AppBundle:datauser')->findOneByIduser($datasearch->getId());
+            $tabsearch[] = array(
+                'gender' => $thisGender->getGenre(),
+                'username' => $datasearch->getUsername(),
+                'lastlogin' => $datasearch->getLastlogin(),
+                'id' => $datasearch->getId(),
+            );
+        }
+
+        return $this->render('search.html.twig', array(
+            'tabsearchs' => $tabsearch,
+        ));
+    }
+
+
+}
